abeb99e5b81fb715ac5cf480b8997faa46d1202b
Edouard Castelnau <edouard.castelnau@gmail.com>
2020-02-17 17:02:08 +0100

    users_stars_db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_141525) do
+ActiveRecord::Schema.define(version: 2020_02_17_155845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stars", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "category"
+    t.integer "price"
+    t.string "sex"
+    t.integer "size"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_stars_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +36,11 @@ ActiveRecord::Schema.define(version: 2020_02_17_141525) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "stars", "users"
 end
