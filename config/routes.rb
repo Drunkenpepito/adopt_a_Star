cedea6aa8a50f99835424a0206ff07dc887ff841
Edouard Castelnau <edouard.castelnau@gmail.com>
2020-02-17 18:20:53 +0100

    star_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,14 @@
 Rails.application.routes.draw do
-  get 'stars/index'
-  get 'stars/show'
-  get 'stars/new'
-  get 'stars/create'
-  get 'stars/edit'
-  get 'stars/update'
-  get 'stars/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :stars do
-    resources :bookings, only: %i[show new create] do
-      resources :reviews
-    end
+    resources :bookings, only: %i[show new create]
   end
 
-  resources :bookings, only: %i[index edit update]
+  resources :bookings, only: %i[index edit update] do
+    resources :reviews
+  end
 
   # resources :bookings, only: %i[show] do
   # end
