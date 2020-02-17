d7a4737894e62122b17e8079153f8e006b890110
pierre furic <pfuric@hotmail.com>
2020-02-17 17:02:41 +0100

    commit'

--- db/migrate/20200217155541_add_columns_to_bookings.rb
+++ db/migrate/20200217155541_add_columns_to_bookings.rb
@@ -1,8 +1,8 @@
 class AddColumnsToBookings < ActiveRecord::Migration[6.0]
   def change
     add_column :bookings, :time, :date
-    add_foreign_key :bookings, :users
-    add_foreign_key :bookings, :stars
+    add_reference :bookings, :user, foreign_key: true
+    add_reference :bookings, :star, foreign_key: true
     add_column :bookings, :from, :date
     add_column :bookings, :to, :date
   end
