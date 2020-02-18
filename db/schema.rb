64ed96072b85d42d1ddf5e49c22fa8510ef0e6f0
Xavier Blanchy <blanchyx@gmail.com>
2020-02-18 12:58:33 +0100

    Add dotenv - Protect my secret data in .env file

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_02_17_155845) do
-=======
 ActiveRecord::Schema.define(version: 2020_02_18_110446) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,10 +80,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_110446) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
-=======
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
->>>>>>> master
   add_foreign_key "reviews", "bookings"
   add_foreign_key "stars", "users"
 end
