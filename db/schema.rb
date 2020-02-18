312f9c67c608a236c7944667b3b07709fb766a02
Xavier Blanchy <blanchyx@gmail.com>
2020-02-18 11:53:56 +0100

    change coucou

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_174030) do
+ActiveRecord::Schema.define(version: 2020_02_17_155845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,10 @@ ActiveRecord::Schema.define(version: 2020_02_17_174030) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "time"
+    t.integer "user_id"
+    t.integer "star_id"
     t.date "from"
     t.date "to"
-    t.bigint "user_id"
-    t.bigint "star_id"
-    t.index ["star_id"], name: "index_bookings_on_star_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -61,8 +59,6 @@ ActiveRecord::Schema.define(version: 2020_02_17_174030) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "bookings", "stars"
-  add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "bookings"
   add_foreign_key "stars", "users"
 end
