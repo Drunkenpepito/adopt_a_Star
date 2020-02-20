5410b756dd7eafdd862ef0ac6670069e6138e901
Benjamin Crot <benjamin.crot@gmail.com>
2020-02-20 12:07:14 +0100

    done

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,6 +1,6 @@
 class Star < ApplicationRecord
   geocoded_by :city
-  has_one_attached :photo
+  has_many_attached :photos
   has_many :bookings
   validate :attached_file?
   after_validation :geocode,  if: :will_save_change_to_city?
