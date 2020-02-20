e814a46cdfa87a3ce84f76b52880b0d4b3ab8d00
Edouard Castelnau <edouard.castelnau@gmail.com>
2020-02-20 15:56:13 +0100

    navbar_sticky_update_forms_sex_to_gender

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_133256) do
+ActiveRecord::Schema.define(version: 2020_02_20_144924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2020_02_19_133256) do
     t.string "city"
     t.string "category"
     t.integer "price"
-    t.string "sex"
+    t.string "gender"
     t.integer "size"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
